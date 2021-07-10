b35b6b8d6239c4d9daccbd2ebc8657cec750556c
Rakesh Kasibhatla <vjyrakesh@gmail.com>
2021-07-10 19:42:32 +0530

    Add message info in help and readme

--- gitscripts/help.bat
+++ gitscripts/help.bat
@@ -3,11 +3,11 @@ echo                         ****************************
 echo                               Git scripts Help
 echo                            (c) Rakesh Kasibhatla
 echo                         ****************************
-echo ga.bat     == git add .
-echo gb.bat     == git branch
-echo gc.bat     == git commit -m [message]
-echo gcb.bat    == git checkout [branch] 
-echo gcnb.bat   == git checkout -b [branch]
-echo gpob.bat   == git push origin [branch]
-echo gpom.bat   == git push origin master
-echo gs.bat     == git status
+echo ga.bat             == git add .
+echo gb.bat             == git branch
+echo gc.bat [message]   == git commit -m [message]
+echo gcb.bat [branch]   == git checkout [branch] 
+echo gcnb.bat [branch]  == git checkout -b [branch]
+echo gpob.bat [branch]  == git push origin [branch]
+echo gpom.bat           == git push origin master
+echo gs.bat             == git status
